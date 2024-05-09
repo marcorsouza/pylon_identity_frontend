549d9c1a39137bacfc78d988ccd0634d76ce95e0
marcorsouza <marco.rsouza@gmail.com>
2024-05-08 23:55:17 -0300

    formato login.tsx

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -4,7 +4,7 @@ import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
 import { Password } from "primereact/password";
 import { Checkbox } from "primereact/checkbox";
-import { Toast } from 'primereact/toast';
+import { Toast } from "primereact/toast";
 
 export default function Login() {
   const [username, setUsername] = useState("");
@@ -13,20 +13,30 @@ export default function Login() {
   const toast = useRef<Toast>(null);
 
   const showSuccess = () => {
-    if(toast.current)
-      toast.current.show({severity:'success', summary: 'Success', detail:'Message Content', life: 3000});
-  }
+    if (toast.current)
+      toast.current.show({
+        severity: "success",
+        summary: "Success",
+        detail: "Message Content",
+        life: 3000,
+      });
+  };
 
   const showError = () => {
-    if(toast.current)
-      toast.current.show({severity:'error', summary: 'Invalid username or password', detail:'Invalid username or password, remaining_attempts: 5', life: 3000});
-  }
-  
+    if (toast.current)
+      toast.current.show({
+        severity: "error",
+        summary: "Invalid username or password",
+        detail: "Invalid username or password, remaining_attempts: 5",
+        life: 3000,
+      });
+  };
+
   const handleLogin = (e: any) => {
     e.preventDefault();
     console.log("Login details:", { username, password, rememberMe });
-    showSuccess()
-    showError()
+    showSuccess();
+    showError();
     // Adicionar lógica de autenticação
   };
 
